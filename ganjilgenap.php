d7c0f16e78c1072d3a4eeb0aec784ea2ab5e9d29
Nadif Rayhan Julio Mohede <91080633+nadifmohede@users.noreply.github.com>
2023-05-03 19:30:00 +0700

    Tambah File

---
+++ ganjilgenap.php
@@ -0,0 +1,11 @@
+<?php
+    for($nomor = 1; $nomor <= 10; $nomor++){
+        if($nomor % 2 == 0){
+            echo $nomor . " adalah bilangan genap <br>";
+        }
+        else{
+            echo $nomor . " adalah bilangan ganjil <br>";
+        }
+    }
+
+?>
